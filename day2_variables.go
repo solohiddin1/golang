64f3d7ab108623bb50e3254f45a5f769c3e0f1c7
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-20 11:10:01 +0500

    READ

--- day2_variables.go
+++ day2_variables.go
@@ -9,17 +9,19 @@ var golang bool
 var a, b int = 1,2
 
 func main(){
+myslice := []int{1,2}
+    
     //s := 1
     //var l int
-    
+fmt.Println(myslice)    
     //l = 's'
     //l = 11
  //   var l1 string
     //fmt.Printf("%d",l)
    // fmt.Printf("%g",l1)
     //fmt.Println("\nthis is text")
-golang = true
-fmt.Println("Python -> ",python, "java=",java)
-fmt.Println("go lang - >",golang)
+//golang = true
+//fmt.Println("Python -> ",python, "java=",java)
+//fmt.Println("go lang - >",golang)
 
 }
