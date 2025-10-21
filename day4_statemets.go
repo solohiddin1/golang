ac50300a0a59bd1259bcca319faacd9e88657256
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-21 21:04:56 +0500

    day4 statements

---
+++ day4_statemets.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt" 
+"math")
+
+func  main(){
+var num int
+fmt.Printf("input number to check=")
+fmt.Scanf("%d",&num)
+pi := math.Pi
+fmt.Printf("pi is- %0.2f \n",pi)
+if num > int(pi) {
+fmt.Println("greater than pi")
+} else{
+fmt.Println("smaller than pi")
+}
+}
