111fee344279c46cf19b01cc3f0f91e26772e0f4
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-21 21:34:16 +0500

    data types

--- day4_statemets.go
+++ day4_statemets.go
@@ -8,8 +8,9 @@ fmt.Printf("input number to check=")
 fmt.Scanf("%d",&num)
 pi := math.Pi
 fmt.Printf("pi is- %0.2f \n",pi)
-if num > int(pi) {
+if float64(num) > pi {
 fmt.Println("greater than pi")
+fmt.Printf("num is %T pi is %T\n", float64(num), pi)
 } else{
 fmt.Println("smaller than pi")
 }
