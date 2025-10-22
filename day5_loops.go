c34ad787fa1d355a8412d3d621e2ef6e7511a436
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-22 22:37:43 +0500

    day5_loop

--- day5_loops.go
+++ day5_loops.go
@@ -6,9 +6,18 @@ func main(){
 //var n int16
 //fmt.Scanf("%d", &n)
 start := time.Now()
-//for i:=0;  i<1000_000_0; i++ {
-//fmt.Printf("i is -> %d\n",i)
-//}
+for i:=0;  i<10; i++ {
+fmt.Printf("i is -> %d\n",i)
+}
+i := 0
+for  i<10{
+fmt.Printf("still in the loop %d\n",i)
+i++
+}
+
+for {
+fmt.Println("this is infinite!")
+}
 res := time.Since(start)
 //fmt.Printf("n is -> %d\n",n)
 fmt.Println("time spent ->",res)
